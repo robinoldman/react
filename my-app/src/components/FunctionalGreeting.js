f4bb9eb75a83efd1a702327451f7f5934a822201
robinoldman <robin.c.oldman@gmail.com>
2023-07-05 10:01:31 +0000

    initial commit

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function FunctionalGreeting() {
+    return <h1> Hellow from React</h1>
+
+}
+
+export default FunctionalGreeting;
+
+
---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FunctionalGreetingWithProps = () => <h1>
+    Hello
+</h1>
+    return <h1> Hello</h1>
+
+
+
+export default FunctionalGreetingWithProps;
+
+
